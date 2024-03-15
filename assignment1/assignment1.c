7a07f5701649a18c67f3ccd869cfeecf441065ee
tjrwls0826 <tjrwls0826@naver.com>
2024-03-15 10:25:19 +0900

    assignment 2

--- assignment1/assignment1.c
+++ assignment1/assignment1.c
@@ -2,7 +2,7 @@
 
 int main(void)
 {
-	char* ptr[4] = { "Korea","Seoul","Mapo","152번지2/3" };
+	char* ptr[4] = { "Korea","Seoul","Mapo","152번지 2/3" };
 
 	printf("%s\n%s\n%s\n%s\n", ptr[0], ptr[1], ptr[2], ptr[3]);
 
---
+++ assignment_2/assignment2.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    char a[100]; 
+    int len = 0;
+
+    printf("문자열을 입력하세요:");
+    scanf("%s", a);
+
+    for (int i = 0; a[i] != '\0'; ++i) {
+        len++;
+    }
+
+    // 입력 받은 문자열과 해당 문자열의 길이 출력
+    printf("\n\n입력한 문자열은\n\"%s\"\n입니다.\n\n", a);
+    printf("입력된 문자열의 길이=%d\n", len);
+
+    return 0;
+}
