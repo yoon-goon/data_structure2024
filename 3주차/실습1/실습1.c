65aa9d548dd318d88b88850a56833ec3d312a8b8
tjrwls0826 <tjrwls0826@naver.com>
2024-03-19 09:21:21 +0900

    3주차 실습1

---
+++ 3주차/실습1/실습1.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+
+
+int main()
+{
+	int i;
+	double start, end;
+
+	//for 루프 시작 시간
+	start = (double)clock() / CLOCKS_PER_SEC;
+
+	//for loop 100000000번 돌아보기
+	int sum = 0;
+	for (i = 0; i < 100000000; i++) {
+		sum++;
+	}
+
+	//for loop end time
+	end = (((double)clock()) / CLOCKS_PER_SEC);
+	printf("program running time :%lf\n", (end - start));
+}
