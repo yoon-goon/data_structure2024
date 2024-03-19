320c29ca396c3caba76f1b31c18edbdc0d3f66f0
tjrwls0826 <tjrwls0826@naver.com>
2024-03-19 10:42:05 +0900

    Update assignment2_3d.c

--- 2주차/assignment_2_3dArray/assignment2_3d.c
+++ 2주차/assignment_2_3dArray/assignment2_3d.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main() {
 	int arr[2][3][4];
 	int num = 0;
 
-	for (int i = 0; i < 2; i++) {
-		for (int y = 0; y < 3; y++) {
-			for (int x = 0; x < 4; x++) {
+	for (int i = 0; i < 2; i++) {// 면
+		for (int y = 0; y < 3; y++) {// 행
+			for (int x = 0; x < 4; x++) {// 열
 				num++;
 				arr[i][y][x] = num;
 				printf("array[%d][%d][%d] = %d\n", i, y, x, arr[i][y][x]);
