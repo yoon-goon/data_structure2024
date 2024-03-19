f2a3f77b091da048aa18330608b2a715c90e5482
tjrwls0826 <tjrwls0826@naver.com>
2024-03-19 10:37:19 +0900

    Update assignment2_3d.c

--- 2주차/assignment_2_3dArray/assignment2_3d.c
+++ 2주차/assignment_2_3dArray/assignment2_3d.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int main() {
+	int arr[2][3][4];
 	int num = 0;
 
 	for (int i = 0; i < 2; i++) {
 		for (int y = 0; y < 3; y++) {
 			for (int x = 0; x < 4; x++) {
 				num++;
-				printf("array[%d][%d][%d] = %d\n", i, y, x, num);
+				arr[i][y][x] = num;
+				printf("array[%d][%d][%d] = %d\n", i, y, x, arr[i][y][x]);
 			}
 		}
 	}
+	getchar();
 }
