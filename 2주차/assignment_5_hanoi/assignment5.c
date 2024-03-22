46cafd810d14a5d31b7797fcef38b80e7a23a18f
tjrwls0826 <tjrwls0826@naver.com>
2024-03-22 22:14:04 +0900

    hanoi recursion

---
+++ 2주차/assignment_5_hanoi/assignment5.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void hanoi(int n, char start, char work, char target)
+{
+    if (n == 1) {
+        printf("\n%c에서 원판 1을 %c로 옮김", start, target);
+    }
+    else
+    {
+        hanoi(n - 1, start, target, work);
+        printf("\n%c에서 원판 %d을 %c로 옮김", start,n, target);
+        hanoi(n - 1, work, start, target);
+    }
+
+}
+
+int main()
+{
+    int num;
+    printf("원반 개수를 입력해주세요: ");
+    scanf("%d", &num);
+
+    hanoi(num, 'A', 'B', 'C');
+}
