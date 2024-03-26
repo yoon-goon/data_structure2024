9d58f35d845bd4b70e186a4417918c54d9e9a3a3
tjrwls0826 <tjrwls0826@naver.com>
2024-03-26 10:47:01 +0900

    피보나치 구현

---
+++ 3주차/실습2fibo/W3P2Fibo.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <time.h>
+#include <stdio.h>
+
+int Fibo(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n < -1) {
+        return 1;
+    }
+    return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main() {
+    int n;
+    int i;
+
+    printf("몇 번 째까지 피보나치 수열을 구할까요? ");
+    scanf_s("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("%d번 째 수 ", i);
+        printf("%d\n", Fibo(i));
+    }
+    return 0;
+}
