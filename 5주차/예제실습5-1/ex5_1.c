5837a43a601b23614aec39f2c53d80cc43a2530d
tjrwls0826 <tjrwls0826@naver.com>
2024-04-30 11:17:44 +0900

    ex 5-1 stack

---
+++ 5주차/예제실습5-1/ex5_1.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include "stackS.h"
+
+int main(void) {
+    element item;
+    printf("\n** 순차 스택 연산 **\n");
+    printStack();
+    push(1); printStack;    // 1push
+    push(2); printStack;    // 2push
+    push(3); printStack;    // 3push
+
+    item = peek();  printStack();	// 현재 top의 원소 출력
+    printf("peek => %d", item);
+
+    item = pop();  printStack();	// 삭제
+    printf("\t pop  => %d", item);
+
+    item = pop();  printStack();	// 삭제
+    printf("\t pop  => %d", item);
+
+    item = pop();  printStack();	// 삭제
+    printf("\t pop  => %d\n", item);
+
+    getchar();  return 0;
+}
