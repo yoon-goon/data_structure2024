f2f7b3387190efc272869b94e1e67b82cdd33bd9
tjrwls0826 <tjrwls0826@naver.com>
2024-04-30 11:31:29 +0900

    피보나치 테스트

---
+++ 중간대비/Fibo/fibo.c
@@ -0,0 +1,26 @@
+﻿#include <time.h>
+#include <stdio.h>
+
+int Fibo(int n)
+{
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+
+
+    return Fibo(n - 1) + Fibo(n - 2);
+}
+
+
+int main(void) {
+    int n;
+    int i;
+
+    printf("몇번째까지?:");
+    scanf_s("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        printf("%d 번 째 수", i);
+        printf("%d\n", Fibo(i));
+    }
+    return 0;
+}
