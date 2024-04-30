138cfcb70108d46b832bf44daadae1f3bf7807f9
tjrwls0826 <tjrwls0826@naver.com>
2024-04-30 11:25:12 +0900

    ex5-1

--- 5주차/예제실습5-1/stackS.c
+++ 5주차/예제실습5-1/stackS.c
@@ -3,7 +3,7 @@
 
 int top = -1;
 
-// 스택이 공백 상태인지 확인하는 연산
+// 스택이 공백 상태확인 연산
 int isStackEmpty() {
     if (top == -1) 
         return 1;
@@ -11,7 +11,7 @@ int isStackEmpty() {
         return 0;
 }
 
-// 스택이 포화 상태인지 확인하는 연산
+// 스택이 포화 상태확인 연산
 int isStackFull() {
     if (top == STACK_SIZE - 1) // ppt에선 n-1
         return 1; 
@@ -19,7 +19,7 @@ int isStackFull() {
         return 0;
 }
 
-// 스택의 top에 원소를 삽입하는 연산
+// 스택의 top에 원소삽입하는 연산
 void push(element item) {
     if (isStackFull()) {        // 스택이 포화 상태인 경우
         printf("\n\n Stack is FULL! \n");
@@ -29,7 +29,7 @@ void push(element item) {
         stack[++top] = item;   // top을 증가시킨 후 현재 top에 원소 삽입
 }
 
-// 스택의 top에서 원소를 삭제하는 연산
+// 스택의 top에서 원소 삭제 연산
 element pop() {
     if (isStackEmpty()) {		// 스택이 공백 상태인 경우
         printf("\n\n Stack is Empty!!\n");
@@ -39,7 +39,7 @@ element pop() {
         return stack[top--];	// 현재 top의 원소를 삭제한 후 top 감소
 }
 
-// 스택의 top 원소를 검색하는 연산
+// 스택의 top 원소검색 연산
 element peek() {
     if (isStackEmpty()) {		// 스택이 공백 상태인 경우
         printf("\n\n Stack is Empty !\n");
@@ -49,7 +49,7 @@ element peek() {
         return stack[top];		// 현재 top의 원소 확인
 }
 
-// 스택의 원소를 출력하는 연산
+// 스택의 원소출력 연산
 void printStack() {
     int i;
     printf("\n STACK [ ");
