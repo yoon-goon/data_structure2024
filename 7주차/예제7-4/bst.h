ab3464ffe319b3230ae226b7a919c7d7df0fb1e6
tjrwls0826 <tjrwls0826@naver.com>
2024-05-21 11:03:08 +0900

    BST practice

---
+++ 7주차/예제7-4/bst.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+#include "node.h"
+
+treeNode* searchBST(treeNode* root, element x);
+treeNode* insertBSTNode(treeNode* p, element x);
+void eleteBSTNode(treeNode* root, element key);
